baea6904184b7ea10878fa473ce3e660318ad817
Samu Ruuhonen <samu.ruuhonen@trimble.com>
2023-11-08 11:42:59 +0200

    Added local nuget build support

---
+++ NuGetter/BuildLocalNuget.bat
@@ -0,0 +1,12 @@
+
+MSBuild.exe .. /p:Configuration=140-DebugCRT /p:Platform=x64
+MSBuild.exe .. /p:Configuration=142-DebugCRT /p:Platform=x64
+MSBuild.exe .. /p:Configuration=140-Release /p:Platform=x64
+MSBuild.exe .. /p:Configuration=142-Release /p:Platform=x64
+
+@echo off
+set /p Build=<ver.txt
+set /a Build+=1
+echo %Build% >ver.txt
+
+c:\work\nuget.exe pack -Version 0.0.%Build%-test zlib-ng.nuspec
